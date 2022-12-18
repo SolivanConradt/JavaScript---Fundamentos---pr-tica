80d0ec301e238223dbf3cbf9b6cb2fe9f98ca176
Solivan Conradt <solicrt07@gmail.com>
2022-12-18 00:53:42 +0000

    06 - entrada de dados

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var salario_bruto = 7500;
+const prompt  = require('prompt-sync')();
+
+var nome = prompt("Digite seu nome: ");
+var salario_bruto = parseFloat(prompt("Digite seu salário bruto: "));
 var imposto_renda = salario_bruto * 0.10;
 var salario_liquido = salario_bruto - imposto_renda;
 console.log("Salário Líquido: R$", salario_liquido);
