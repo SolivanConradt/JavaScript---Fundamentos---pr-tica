5fb213e1a793b2cdecfb21c0fa21460e13494b74
Solivan Conradt <solicrt07@gmail.com>
2022-12-25 14:07:45 +0000

    09 - Comandos de Repetição

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const prompt = require('prompt-sync')();
 
-var nome = prompt("Digite seu nome: ");
-var salario_bruto = parseFloat(prompt("Digite seu salário bruto: "));
-var dependente = parseInt(prompt("Digite o número de dependentes: "));
+for (var i = 1; i <= 5; i++) {
+  console.log("Pessoa ", i);
 
-var renda_percapta = salario_bruto / (dependente + 1);
+  var nome = prompt("Digite seu nome: ");
+  var salario_bruto = parseFloat(prompt("Digite seu salário bruto: "));
+  var dependente = parseInt(prompt("Digite o número de dependentes: "));
 
-if (renda_percapta >= 500) {
+  var renda_percapta = salario_bruto / (dependente + 1);
 
-  if (salario_bruto > 0 && salario_bruto <= 1903.98) {
-    var imposto_renda = salario_bruto * 0.05;
-  } else if (salario_bruto <= 2826.65) {
-    var imposto_renda = salario_bruto * 0.075;
+  if (renda_percapta >= 500) {
+
+    if (salario_bruto > 0 && salario_bruto <= 1903.98) {
+      var imposto_renda = salario_bruto * 0.05;
+    } else if (salario_bruto <= 2826.65) {
+      var imposto_renda = salario_bruto * 0.075;
+    } else {
+      var imposto_renda = salario_bruto * 0.15;
+    }
+
   } else {
-    var imposto_renda = salario_bruto * 0.15;
+    var imposto_renda = 0;
   }
 
-} else {
-  var imposto_renda = 0;
-}
+  var salario_liquido = salario_bruto - imposto_renda;
+  console.log("Salário Líquido: R$", salario_liquido, "\n");
 
-var salario_liquido = salario_bruto - imposto_renda;
-console.log("Salário Líquido: R$", salario_liquido);
+}
